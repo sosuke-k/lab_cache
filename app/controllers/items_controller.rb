f4e33f33fc783cdf428195051cdbf47df7d4db72
daichi <daipanchi@gmail.com>
2014-04-25 23:41:19 +0900

    Delete comments

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,41 +2,19 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   include ApplicationHelper
-  # GET /items
-  # GET /items.json
   def index
     @items = Item.all
   end
 
-  # GET /items/1
-  # GET /items/1.json
   def show
   end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit
   end
-
-  # POST /items
-  # POST /items.json
-  # def create
-  #   @item = Item.new(item_params)
-
-  #   respond_to do |format|
-  #     if @item.save
-  #       format.html { redirect_to @item, notice: 'Item was successfully created.' }
-  #       format.json { render action: 'show', status: :created, location: @item }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @item.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   def create
     require 'open-uri'
@@ -56,7 +34,6 @@ class ItemsController < ApplicationController
 
     twitter_id = params[:user][:quiche_twitter_id]
     image_url = params[:user][:quiche_twitter_image_url]
-
 
     if ( ( user = User.find_by(twitter_id: twitter_id) ) == nil )
       user = User.create({
@@ -94,8 +71,6 @@ class ItemsController < ApplicationController
     Reader.create({user: user, item: item}) # user を reader に追加
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
     respond_to do |format|
       if @item.update(item_params)
@@ -108,8 +83,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item.destroy
     respond_to do |format|
@@ -119,12 +92,10 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:title, :first_image_url, :user_id, :name, :content, :deleted_at)
     end
