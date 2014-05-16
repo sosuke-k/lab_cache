a0427610d45747668532477abc67421a3a3e2422
daichi <daipanchi@gmail.com>
2014-05-16 19:55:04 +0900

    Move delete-button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,6 @@
 $(window).load(function() {
   $('.comment_button').click(function() {
     event.preventDefault();
-    $(this).parent().children('.modal').modal()
+    $(this).parent().parent().children('.modal').modal()
   })
 })
